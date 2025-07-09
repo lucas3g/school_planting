83cfa10bfa3f29d37b7fee33d7f762ab02e3a204
Lucas Emanuel Silva <53888064+lucas3g@users.noreply.github.com>
2025-07-09 16:12:27 -0300

    Use Isolate to compress images

--- lib/shared/utils/image_utils.dart
+++ lib/shared/utils/image_utils.dart
@@ -1,15 +1,18 @@
 import 'dart:io';
+import 'dart:isolate';
 
 import 'package:image/image.dart' as img;
 
-Future<File> compressImage(File file, {int quality = 80}) async {
-  final bytes = await file.readAsBytes();
+Future<File> compressImage(File file, {int quality = 80}) {
+  return Isolate.run(() {
+    final bytes = file.readAsBytesSync();
 
-  final image = img.decodeImage(bytes);
+    final image = img.decodeImage(bytes);
 
-  if (image == null) return file;
+    if (image == null) return file;
 
-  final compressedBytes = img.encodeJpg(image, quality: quality);
+    final compressedBytes = img.encodeJpg(image, quality: quality);
 
-  return file..writeAsBytesSync(compressedBytes);
+    return file..writeAsBytesSync(compressedBytes);
+  });
 }
