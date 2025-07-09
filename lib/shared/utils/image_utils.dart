14e20b646a9e2cd23f427392cf10c9d12ee336aa
Lucas Emanuel Silva <53888064+lucas3g@users.noreply.github.com>
2025-07-09 15:34:20 -0300

    Compress images before upload

---
+++ lib/shared/utils/image_utils.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+import 'package:image/image.dart' as img;
+
+Future<File> compressImage(File file, {int quality = 80}) async {
+  final bytes = await file.readAsBytes();
+  final image = img.decodeImage(bytes);
+  if (image == null) return file;
+  final compressedBytes = img.encodeJpg(image, quality: quality);
+  return file..writeAsBytesSync(compressedBytes);
+}
+
